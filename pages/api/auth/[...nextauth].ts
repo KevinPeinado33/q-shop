1c98a87cdf0c11050a4825025eaec567c00e5522
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2023-03-03 09:53:13 -0500

    se queda aqui

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,12 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
+const client_id = process.env.GITHUB_ID
+const client_secret = process.env.GITHUB_SECRET
+const nextauth_url = process.env.NEXTAUTH_URL
+
+console.log({ client_id, client_secret, nextauth_url })
+
 export default NextAuth({
     providers: [
         GithubProvider({
